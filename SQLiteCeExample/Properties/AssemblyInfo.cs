17bb0f6eec09a74bf05446022204bcdfb323a2ab
Wachirawit Chankaew <wachirawit.chankaew@gmail.com>
2017-11-26 09:07:44 +0700

    Create project.

---
+++ SQLiteCeExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SQLiteCeExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SQLiteCeExample")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ad43a4fb-2643-45d9-b06b-066cb026733b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// Below attribute is to suppress FxCop warning "CA2232 : Microsoft.Usage : Add STAThreadAttribute to assembly"
+// as Device app does not support STA thread.
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2232:MarkWindowsFormsEntryPointsWithStaThread")]
---
+++ SQLiteCeExample/Repository/Database.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Data.SqlServerCe;
+
+namespace SQLiteCeExample.Repository
+{
+    /// <summary>
+    /// https://blogs.msdn.microsoft.com/windows-embedded/2013/02/14/sqlite-and-windows-embedded-compact/
+    /// </summary>
+    public class Database
+    {
+        private const string db = "test.db";
+        private const string password = "asdf";
+
+        private readonly string  dbPath;
+
+        public Database()
+        {
+            string path = System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase;
+            string dir = Path.GetDirectoryName(path);
+            dbPath = Path.Combine(dir, db);
+        }
+
+        public void CreateDb()
+        {
+            if (File.Exists(dbPath))
+            {
+                File.Delete(dbPath);
+            }
+
+            string connectionString = string.Format("DataSource='{0}'; LCID=1033; Password='{1}'", dbPath, password);
+            using (SqlCeEngine engine = new SqlCeEngine(connectionString))
+            {
+                engine.CreateDatabase();
+            }
+        }
+    }
+}
